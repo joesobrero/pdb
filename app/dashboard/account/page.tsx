43a3d2e87b1c8c4a4994c99bc687d4bf93daf7e2
Joseph Sobrero <joesobrero@gmail.com>
2025-02-27 15:29:37 -0800

    loading state for acct email

--- app/dashboard/account/page.tsx
+++ app/dashboard/account/page.tsx
@@ -1,29 +1,38 @@
 "use client";
 
-import { useEffect, useState } from "react";
 import { createClient } from "@/lib/supabase/client";
-import { User } from "@supabase/auth-helpers-nextjs";
 import Button from "@/app/components/interactive/button";
 import { faSignOut, faTrash } from "@fortawesome/pro-regular-svg-icons";
+import { useEffect, useState } from "react";
 
 const AccountPage = () => {
-  const [user, setUser] = useState<User | null>(null);
   const supabase = createClient();
+  const [email, setEmail] = useState<string | null>(null);
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
-    const getUser = async () => {
-      const { data } = await supabase.auth.getUser();
-      setUser(data.user);
+    const fetchEmail = async () => {
+      try {
+        const { data } = await supabase.auth.getUser();
+        setEmail(data.user?.email ?? null);
+      } catch (error) {
+        console.error("Error fetching email:", error);
+      } finally {
+        setIsLoading(false);
+      }
     };
-    getUser();
-  }, [supabase.auth]);
+
+    fetchEmail();
+  }, [supabase]);
 
   return (
     <>
       <h1 className="text-3xl font-display font-medium">Account</h1>
       <div className="flex flex-col gap-2">
         <p className="text-sm text-muted">Email</p>
-        <p className="font-medium">{user?.email}</p>
+        <p className="font-medium">
+          {isLoading ? <span className="text-muted">Loading...</span> : email}
+        </p>
       </div>
       <div className="flex flex-row gap-2">
         <Button
